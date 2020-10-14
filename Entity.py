d46dd41c1e672a25f862b6fbc8adbed913836b47
Neandrewthal <agacek333@gmail.com>
2020-10-14 14:50:22 -0500

    Started making entity

--- Entity.py
+++ Entity.py
@@ -1,3 +1,38 @@
 from Constants import *
+import copy
 
+class Entity:
+
+	def __init__(self, entity_name, entity_score, entity_profile):
+		self.name = entity_name
+		self.score = entity_score
+		self.profile = copy.deepcopy(entity_profile)
+		self.alive = True
+		self.parse_entity_profile # For now, profiles will be stored in Constants
+
+	def parse_entity_profile(profile):
+		self.level = profile['level']
+
+		self.max_hp = profile['max_hp']
+		if 'hp' in profile:
+			self.hp = profile['hp']
+		else:
+			self.hp = self.max_hp
+
+		self.max_mp = profile['max_mp']
+		if 'mp' in profile:
+			self.mp = profile['mp']
+		else:
+			self.mp = self.max_mp
+
+		for equipment in profile['equipment']:
+			self.add_equipment(equipment)
+
+	def damage(self, amount):
+		self.hp -= amount
+		if self.hp <= 0:
+			self.die()
+
+	def die(self):
+		self.alive=False
 
